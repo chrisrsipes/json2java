2ea54064223ac4ca0433d9b1e72109317a634319
Christopher Sipes <crs@Christophers-MacBook-Pro.local>
2018-08-09 21:09:13 -0400

    Added support for other TokenTypes

--- src/main/java/com/crs/utils/CodeUtils.java
+++ src/main/java/com/crs/utils/CodeUtils.java
@@ -36,6 +36,39 @@ public class CodeUtils {
                     String newVariableCreated = generateCodeHelper(childTokenPair.getRight(), childTokenPair.getLeft(), nameCounts);
                     System.out.println(String.format("%s.set%s(%s);", variableCreated, getClassName(childTokenPair.getRight().getKey()), newVariableCreated));
                 }
+
+                break;
+            case LIST:
+                ListToken listToken = (ListToken) token;
+                variableCreated = getVariableName(listToken.getKey() + "List", nameCounts);
+                System.out.println(String.format("List<%s> %s = new ArrayList<%s>();", getClassName(listToken.getKey()), variableCreated, getClassName(listToken.getKey())));
+
+                for (ImmutablePair<TokenType, Token> childTokenPair : listToken.getChildTokens()) {
+                    childTokenPair.getRight().setKey(getClassName(listToken.getKey()));
+                    String newVariableCreated = generateCodeHelper(childTokenPair.getRight(), childTokenPair.getLeft(), nameCounts);
+                    System.out.println(String.format("%s.add(%s);", variableCreated, newVariableCreated));
+                }
+
+                break;
+            case STRING:
+                StringToken stringToken = (StringToken) token;
+                variableCreated = getVariableName(stringToken.getKey(), nameCounts);
+                System.out.println(String.format("String %s = \"%s\";", variableCreated, stringToken.getValue()));
+                break;
+            case PRIMITIVE_DOUBLE:
+                PrimitiveToken primitiveToken1 = (PrimitiveToken) token;
+                variableCreated = getVariableName(primitiveToken1.getKey(), nameCounts);
+                System.out.println(String.format("double %s = %s;", variableCreated, primitiveToken1.getValue()));
+                break;
+            case PRIMITIVE_INT:
+                PrimitiveToken primitiveToken2 = (PrimitiveToken) token;
+                variableCreated = getVariableName(primitiveToken2.getKey(), nameCounts);
+                System.out.println(String.format("int %s = %s;", variableCreated, primitiveToken2.getValue()));
+                break;
+            case PRIMITIVE_LONG:
+                PrimitiveToken primitiveToken3 = (PrimitiveToken) token;
+                variableCreated = getVariableName(primitiveToken3.getKey(), nameCounts);
+                System.out.println(String.format("long %s = %sl;", variableCreated, primitiveToken3.getValue()));
                 break;
         }
 
